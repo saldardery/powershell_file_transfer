b5c1d5fe1ba14150612307ad0eda0947f01eb164
saldardery <139008040+saldardery@users.noreply.github.com>
2023-09-05 05:24:33 -0700

    Add files via upload

---
+++ SCRIPT.ps1
@@ -0,0 +1,17 @@
+﻿
+$hostname = Read-Host -Prompt "Enter the IP/FQDN of the SDDC manager"
+$username = Read-Host -Prompt "Enter the username of the SDDC manager"
+
+function sendfile ($filename)
+{
+scp.exe  $filename $username"@"$hostname":/home/vcf"
+}
+
+function sendfolder
+{
+scp.exe ./testfolder/* $username"@"$hostname":/home/vcf"
+}
+
+#$fn= Read-Host -Prompt "Enter filename"
+#sendfile($fn)
+sendfolder
